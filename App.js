7d70bb5007065184b9e50ccc084c51749a71659e
Juan C Llanos <jllanos@betternoi.com>
2022-06-29 17:49:58 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Dashboard, Place } from "./screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
 
-export default function App() {
+import Tabs from "./navigation/tabs";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  const cacheFonts = (fonts) => {
+    return fonts.map((font) => Font.loadAsync(font));
+  };
+
+  const loadAssetsAsync = async () => {
+    const fontAssets = cacheFonts([
+      { "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf") },
+      { "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf") },
+      { "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf") },
+    ]);
+    await Promise.all([...fontAssets]);
+  };
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadAssetsAsync}
+        onFinish={() => {
+          setIsReady(true);
+        }}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"Dashboard"}
+      >
+        <Stack.Screen name="Dashboard" component={Tabs} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Stack.Screen name="Place" component={Place} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/index.js
@@ -0,0 +1,4 @@
+import Dashboard from "./Dashboard";
+import Place from "./Place";
+
+export { Dashboard, Place };
